54dcfc1fbf40a2ce10488dba42488e6df3b13cb9
Pravar Anu <pravaranu6@gmail.com>
2023-06-13 15:03:39 +0530

    Some changes in Assignment 12

--- DSA/Linked List/Assignment 12/Question06.cpp
+++ DSA/Linked List/Assignment 12/Question06.cpp
@@ -1,13 +1,76 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class ListNode{
-public:
-    int val;
-    ListNode *next;
-    ListNode(): val(0), next(NULL) {}
-    ListNode(int x): val(x) {}
-    ListNode(int x, ListNode *next): val(x), next(next) {}
+class Node {
+  public:
+    int data;
+    Node *next;
 };
 
-int main() { return 0; }
+void push(Node **head_ref, int new_data) {
+    Node *new_node = new Node();
+
+    new_node->data = new_data;
+
+    new_node->next = (*head_ref);
+
+    (*head_ref) = new_node;
+}
+
+void printList(Node *head) {
+    Node *temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void skipMdeleteN(Node *head, int M, int N) {
+    Node *curr = head, *t;
+    int count;
+
+    while (curr) {
+        for (count = 1; count < M && curr != NULL; count++)
+            curr = curr->next;
+
+        if (curr == NULL)
+            return;
+
+        t = curr->next;
+        for (count = 1; count <= N && t != NULL; count++) {
+            Node *temp = t;
+            t = t->next;
+            free(temp);
+        }
+
+        curr->next = t;
+
+        curr = t;
+    }
+}
+
+int main() {
+    Node *head = NULL;
+    int M = 2, N = 3;
+    push(&head, 10);
+    push(&head, 9);
+    push(&head, 8);
+    push(&head, 7);
+    push(&head, 6);
+    push(&head, 5);
+    push(&head, 4);
+    push(&head, 3);
+    push(&head, 2);
+    push(&head, 1);
+
+    cout << "M = " << M << " N = " << N << "\nGiven Linked list is :\n";
+    printList(head);
+
+    skipMdeleteN(head, M, N);
+
+    cout << "\nLinked list after deletion is :\n";
+    printList(head);
+
+    return 0;
+}
